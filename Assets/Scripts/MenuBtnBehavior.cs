a0c1d73131fdb3a4e051919ec40f49968c53fd4b
Ewakhine <Ewakhine>
2017-01-12 22:09:28 +0100

    Add a menu scene with only UI.

---
+++ Assets/Scripts/MenuBtnBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBtnBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMouseOnButton();
+    }
+
+    void UpdateMouseOnButton() {
+        bool mouseOnButton = RectTransformUtility.RectangleContainsScreenPoint(GetComponent<RectTransform>(), Input.mousePosition);
+        if (mouseOnButton) {
+            GetComponents<AudioSource>()[0].Play();
+            GetComponents<AudioSource>()[1].Play();
+        }
+        else {
+            GetComponents<AudioSource>()[0].Stop();
+            GetComponents<AudioSource>()[1].Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Button centralBtn;
+    public Button topBtn;
+    public Button leftBtn;
+    public Button rightBtn;
+
+    public GameObject confirmationPanel;
+    Button yesBtn;
+    Button noBtn;
+
+	// Use this for initialization
+	void Start () {
+        InitializeConfirmationsButtons();
+        InitializeMenuButtons();
+    }
+    /*********************************************************/
+
+    // Update is called once per frame
+    void Update () {
+    }
+    /*********************************************************/
+
+    void InitializeConfirmationsButtons() {
+        yesBtn = confirmationPanel.transform.GetChild(0).GetComponent<Button>();
+        noBtn = confirmationPanel.transform.GetChild(1).GetComponent<Button>();
+
+        confirmationPanel.SetActive(false);
+    }
+    /*********************************************************/
+
+    void InitializeMenuButtons() {
+        leftBtn.interactable = false;
+        rightBtn.interactable = false;
+        centralBtn.onClick.AddListener(() => Btn_LaunchNewGame());
+    }
+    /*********************************************************/
+
+    void Btn_LaunchNewGame() {
+        SceneManager.LoadScene(1);
+    }
+    /*********************************************************/
+
+    public void Btn_OpenConfirmationPanel() {
+        confirmationPanel.SetActive(true);
+    }
+    /*********************************************************/
+
+    public void Btn_CloseConfirmationPanel() {
+        confirmationPanel.SetActive(false);
+    }
+    /*********************************************************/
+
+    public void Btn_QuitGame() {
+        Application.Quit();
+    }
+    /*********************************************************/
+}
